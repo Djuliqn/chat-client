d9d3c0c559529b864fb2b7b10003decaed985667
Stan05 <stanislav9505@gmail.com>
2018-11-09 16:48:30 +0200

    First connection

--- src/main/java/com/chatty/controller/MainController.java
+++ src/main/java/com/chatty/controller/MainController.java
@@ -1,18 +1,57 @@
 package com.chatty.controller;
 
+import com.chatty.util.ParameterStringBuilder;
 import de.felixroske.jfxsupport.FXMLController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 @FXMLController
 public class MainController {
 
     @FXML
-    private Button openSocket;
+    private Button login;
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField password;
 
     @FXML
     private void openSocketClicked(){
-        openSocket.setOnMouseClicked(event ->
-                System.out.print("init"));
+        login.setOnMouseClicked(event -> {
+            try {
+                URL url = new URL("http://localhost:8080/spring-security-rest/login");
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("POST");
+                con.setRequestProperty("Content-Type", "application/json");
+
+                Map<String, String> parameters = new HashMap<>();
+                parameters.put("username", username.getText());
+                parameters.put("password", password.getText());
+
+                con.setDoOutput(true);
+                DataOutputStream out = new DataOutputStream(con.getOutputStream());
+                out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
+                out.flush();
+                out.close();
+
+                con.connect();
+
+                System.out.println(con.getResponseCode());
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.print("Login " + username.getText() + " : " + password.getText());
+        });
     }
 }
